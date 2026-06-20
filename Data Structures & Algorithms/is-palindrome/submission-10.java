855e04a2fa96e7d3b3aa01e70632d75226772c13
khongthichcode <hmquang9805@gmail.com>
2026-06-20 14:28:39 +0700

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length()<=1) return true;
+        StringBuilder sb = new StringBuilder();
+        for(char c: s.toCharArray()){
+            if(Character.isLetterOrDigit(c)) sb.append(Character.toLowerCase(c));
+        }
+        int l = 0;
+        int r = sb.length()-1;
+        while(l<=r){
+            if(sb.charAt(l)!= sb.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
